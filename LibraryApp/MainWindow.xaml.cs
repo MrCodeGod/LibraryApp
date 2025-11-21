603e4c2186902f12409be55d9cb3b54b1f7b063d
nekit <nekit@NEKIT>
2025-11-22 04:27:06 +0500

    Добавьте файлы проекта.

---
+++ LibraryApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace LibrarySimple
+{
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString =
+            "Server=localhost;Database=library_simple;Uid=root;Pwd=Enter_Password_Here!;SslMode=Preferred;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadBooks();
+        }
+
+        private void LoadBooks()
+        {
+            using (var conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string sql = @"
+                    SELECT 
+                        books.id,
+                        books.title AS 'Название',
+                        authors.name AS 'Автор',
+                        genres.name AS 'Жанр'
+                    FROM books
+                    LEFT JOIN authors ON books.author_id = authors.id
+                    LEFT JOIN genres ON books.genre_id = genres.id;
+                ";
+
+                var cmd = new MySqlCommand(sql, conn);
+
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+
+                BooksGrid.ItemsSource = table.DefaultView;
+            }
+        }
+    }
+}
